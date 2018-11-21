2a6d533ee9dee7acea014f0acb42c29e961ed813
Shubham Mathur <shubham.mathur@engineer.com>
2018-11-21 15:51:37 +0530

    Added fetchCartSaga

---
+++ src/sagas/fetchCartSaga.js
@@ -0,0 +1,11 @@
+import fetch from 'isomorphic-fetch';
+import { take, put, apply } from 'redux-saga/effects';
+import { SET_CURRENT_USER, setCartItems } from '../actions';
+
+export function* fetchCartSaga() {
+  const { user } = yield take(SET_CURRENT_USER);
+  const { id } = user;
+  const response = yield fetch(`http://localhost:8081/cart/${id}`);
+  const { items } = yield apply(response, response.json);
+  yield put(setCartItems(items));
+}
