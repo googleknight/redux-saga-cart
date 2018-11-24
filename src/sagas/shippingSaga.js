6070c57baee672b22207ce5a35e152ecf43557cf
Shubham Mathur <shubham.mathur@engineer.com>
2018-11-24 13:07:46 +0530

    Added ShippingSaga

---
+++ src/sagas/shippingSaga.js
@@ -0,0 +1,32 @@
+import { select, put, takeLatest, apply } from 'redux-saga/effects';
+
+import fetch from 'isomorphic-fetch';
+import {
+  SET_CART_ITEMS,
+  INCREASE_ITEM_QUANTITY,
+  DECREASE_ITEM_QUANTITY,
+  FETCHED,
+  FETCHING,
+  setShippingFetchStatus,
+  setShippingCost } from '../actions';
+
+import { cartItemsSelector } from '../selectors';
+
+export function* shipping() {
+  yield put(setShippingFetchStatus(FETCHING));
+  const items = yield select(cartItemsSelector);
+  const itemRequestString = items.reduce((string, item) => {
+    for (let i = 0; i < item.get('quantity'); i += 1) {
+      string += `${item.get('id')},`;
+    }
+    return string;
+  }, '').replace(/,\s*$/, '');
+  const response = yield fetch(`http://localhost:8081/shipping/${itemRequestString}`);
+
+  const { total } = yield apply(response, response.json);
+  yield put(setShippingCost(total));
+  yield put(setShippingFetchStatus(FETCHED));
+}
+export function* shippingSaga() {
+  yield takeLatest([SET_CART_ITEMS, INCREASE_ITEM_QUANTITY, DECREASE_ITEM_QUANTITY], shipping);
+}
